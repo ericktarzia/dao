5072bb9bb8b7a95ed5bbfc653a3922b1c2a5889a
Erick Tarzia <ericktarzia@gmail.com>
2018-02-21 19:59:49 -0300

    Update Sql.class.php

--- class/Sql.class.php
+++ class/Sql.class.php
@@ -15,7 +15,7 @@ class Sql extends PDO
 	 */
 	public function __construct()
 	{
-		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "091105Sophi@");
+		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
 	}
 
 /**
